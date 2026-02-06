b41dbafb718cbb76d130cddd9f90639eb793ddb1
Gabby <gabbzf@gmail.com>
2026-02-06 12:39:41 +0100

    fixed base mode in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: mode === 'production' ? '/portfolio-react/' : '/',
+  base: mode === 'production' ? '/PortfolioSite/' : '/',
   define: {
-    __DEV__: mode === 'development', 
-  }
-}))
+    __DEV__: mode === 'development',
+  },
+}));
