bea155904a10e6f672ad996c780c44d06ec6656c
Gabby <gabbzf@gmail.com>
2026-02-06 12:43:28 +0100

    Change production base path in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: mode === 'production' ? '/portfolio-react/' : '/',
+  base: mode === 'production' ? '/PortfolioSite/' : '/',
   define: {
     __DEV__: mode === 'development', 
   }
